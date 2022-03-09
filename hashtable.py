aa2d375ca2dcf2aaa0d447c8830ad73782f0aab2
zchoi22 <88288951+zchoi22@users.noreply.github.com>
2022-03-08 18:33:35 -0700

    added comments and test driver

--- hashtable.py
+++ hashtable.py
@@ -1,10 +1,13 @@
 # Hashtable class/driver
+#Zion Choi :: 3/7/2022
 
 class hashtable:
+    #constructor => sets capacity and creates buckets
     def __init__(self, capacity):
         self.capacity = capacity
         self.buckets = [[] for i in range(capacity)]
 
+    #checks if bucket is empty, and appends key/value pair if empty
     def put(self, key, value):
         index = self.hashCode(key)
         if self.buckets[index] == []:
@@ -12,6 +15,7 @@ class hashtable:
             return True
         return False
 
+    #returns value using key hashing method
     def get(self, key):
         index = self.hashCode(key)
         try:
@@ -19,12 +23,21 @@ class hashtable:
         except:
             return None
 
+    #converts key to string, then to ASCII characters, finally uses bit AND function
     def hashCode(self, key):
         hash_key = ''
         for char in str(key):
             hash_key+=str(ord(char))
         return int(hash_key) & (self.capacity-1)
 
+#TEST METHOD
+print("==== Creating Hashtable ====")
 test = hashtable(16)
-print(test.put(1, 2))
-print(test.get(1))
+print("==== Hashtable Finalized ====")
+print("\n==== Testing Put Method ====")
+print("Adding key/value pair (1,2): " + str(test.put(1, 2)))
+print("Adding key/value pair (1,5): " + str(test.put(1, 5)))
+print("Adding key/value pair ('hi',198): " + str(test.put('hi', 198)))
+print("\n==== Testing Get Method ====")
+print("Retrieving from key = 1: " + str(test.get(1)))
+print("Retrieving from key = 1: " + str(test.get('hi')))
